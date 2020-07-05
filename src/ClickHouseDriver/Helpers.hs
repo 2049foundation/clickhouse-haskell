f771d8b04d00100114c2e69736e33eb5b6189618
MaboroshiChan <31759226+MaboroshiChan@users.noreply.github.com>
2020-07-05 04:51:20 -0700

    feat: add tcp connection

--- src/ClickHouseDriver/Helpers.hs
+++ src/ClickHouseDriver/Helpers.hs
@@ -1,15 +1,17 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances    #-}
 module ClickHouseDriver.Helpers (
     extract,
     repl,
-    genUrl
+    toStrict,
+    UrlType (..)
 ) where 
 
 import           ClickHouseDriver.Types 
 import qualified Data.Aeson                as JP
-import           Data.Attoparsec.Lazy      (parse)
-import           Data.Attoparsec.Lazy      (Result(..))
-import qualified Data.ByteString           as BS
-import           Data.ByteString.Lazy      (ByteString)
+import           Data.Attoparsec.ByteString     
+import qualified Data.ByteString           as B
+import qualified Data.ByteString.Lazy      as BL
 import           Data.Hashable
 import qualified Data.HashMap.Strict       as HM
 import qualified Data.Text                 as T
@@ -18,10 +20,14 @@ import           Data.Text.Internal.Lazy   (Text)
 import           Data.Text.Lazy.Encoding
 import           Data.Typeable
 import           Data.Vector               (toList)
-
+import qualified Data.ByteString.Lazy.Internal as BLI
+import qualified Data.ByteString.Internal      as BI
+import           Foreign.ForeignPtr
+import           Foreign.Ptr
+import qualified Data.ByteString.Char8         as C8
 
 -- | Trim JSON data
-extract :: ByteString->QueryResult
+extract :: C8.ByteString->JSONResult
 extract val = do
     let res = (parse JP.json (val))
     case res of
@@ -44,11 +50,30 @@ repl []       = []
 repl (' ':cs) = "%20" ++ repl cs
 repl (x:xs)   = x: repl xs
 
--- | generate url
-genUrl :: ClickHouseConnection->Cmd->Url
-genUrl ClickHouseConnectionSettings{ciHost=host,ciPassword=pw,ciPort=port,ciUsername=usr} cmd =
+toStrict :: BL.ByteString -> B.ByteString
+toStrict BLI.Empty = B.empty
+toStrict (BLI.Chunk c BLI.Empty) = c
+toStrict lb = BI.unsafeCreate len $ go lb
+    where
+    len = BLI.foldlChunks (\l sb -> l + B.length sb) 0 lb
+    go  BLI.Empty _   = return ()
+    go (BLI.Chunk (BI.PS fp s l) r) ptr =
+        withForeignPtr fp $ \p -> do
+            BI.memcpy ptr (p `plusPtr` s) (fromIntegral l)
+            go r (ptr `plusPtr` l)
+
+genURL :: ClickHouseConnection->Cmd->String
+genURL HttpConnection{httpHost=host,httpPassword=pw,httpPort=port,httpUsername=usr} cmd =
     let basic = "http://" ++ usr ++ ":" ++ pw ++"@"++ host ++ ":" ++ (show port) ++ "/?query="
         res = basic ++ (repl cmd) in
     res
 
-genUrl (DirectUrl url) cmd = url ++ repl cmd 
+class UrlType a where
+    genUrl :: ClickHouseConnection->Cmd->a
+    genTcp :: ClickHouseConnection->Cmd->a
+-- TODO
+instance UrlType String where
+    genUrl = genURL
+-- TODO
+instance UrlType C8.ByteString where
+    genUrl http cmd = C8.pack $ genURL http cmd
