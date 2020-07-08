ea31c3266dbf51a8a7b017231530f9c6ce97aa63
Darth <bttfus@gmail.com>
2020-07-07 18:25:02 -0700

    style: use ormolu for formatting

--- src/ClickHouseDriver/Default.hs
+++ src/ClickHouseDriver/Default.hs
@@ -1,32 +1,36 @@
-module ClickHouseDriver.Default(
-    usr,
+module ClickHouseDriver.Default
+  ( usr,
     hst,
     pw,
     prt,
-    defaultHttpConnection
-) where
+    defaultHttpConnection,
+  )
+where
 
-import           ClickHouseDriver.Types
-import           Data.ByteString.Internal
-import           Network.HTTP.Client
+import ClickHouseDriver.Types
+import Data.ByteString.Internal
+import Network.HTTP.Client
 
 {-default HTTP settings-}
 usr = "default"
-hst = "localhost"
-pw = ""
-prt = 8123
 
+hst = "localhost"
+
+pw = ""
+
+prt = 8123
 
 defaultHttpConnection :: IO (ClickHouseConnection)
 defaultHttpConnection = do
-    mng <- newManager defaultManagerSettings
-    return HttpConnection {
-     httpHost = hst
-    ,httpPassword = pw
-    ,httpPort = prt
-    ,httpUsername = usr
-    ,httpManager  = mng
-  }
+  mng <- newManager defaultManagerSettings
+  return
+    HttpConnection
+      { httpHost = hst,
+        httpPassword = pw,
+        httpPort = prt,
+        httpUsername = usr,
+        httpManager = mng
+      }
 
 defaultTCPConnection :: IO (ClickHouseConnection)
 defaultTCPConnection = undefined
--- src/ClickHouseDriver/Query.hs
+++ src/ClickHouseDriver/Query.hs
@@ -1,15 +1,16 @@
-{-# LANGUAGE ApplicativeDo              #-}
-{-# LANGUAGE DeriveDataTypeable         #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE StandaloneDeriving         #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE TypeSynonymInstances       #-}
-module ClickHouseDriver.Query (
-    settings,
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module ClickHouseDriver.Query
+  ( settings,
     setupEnv,
     runQuery,
     getByteString,
@@ -17,103 +18,120 @@ module ClickHouseDriver.Query (
     getText,
     getTextM,
     getJsonM,
-) where
+  )
+where
 
-
-import           ClickHouseDriver.Helpers
-import           ClickHouseDriver.Types
-import           Control.Concurrent.Async
-import           Control.Exception
-import qualified Data.Aeson                as JP
-import qualified Data.Attoparsec.Lazy      as AP
-import qualified Data.Attoparsec.Lazy      as DAL
-import qualified Data.ByteString           as BS
-import qualified Data.ByteString.Lazy      as LBS
-import           Data.Hashable
-import qualified Data.HashMap.Strict       as HM
-import qualified Data.Text                 as T
-import           Data.Text.Encoding
-import           Data.Text.Internal.Lazy   (Text)
-import           Data.Typeable
-import           Haxl.Core
-import           Network.HTTP.Client       (Manager, defaultManagerSettings,
-                                            httpLbs, newManager, parseRequest,
-                                            responseBody)
-import           Network.HTTP.Types.Status (statusCode)
-import           Network.Simple.TCP
-import           Network.Socket            (SockAddr, Socket)
-import           Text.Parsec
-import           Text.Printf
+import ClickHouseDriver.Helpers
+import ClickHouseDriver.Types
+import Control.Concurrent.Async
+import Control.Exception
+import qualified Data.Aeson as JP
+import qualified Data.Attoparsec.Lazy as AP
+import qualified Data.Attoparsec.Lazy as DAL
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.HashMap.Strict as HM
+import Data.Hashable
+import qualified Data.Text as T
+import Data.Text.Encoding
+import Data.Text.Internal.Lazy (Text)
+import Data.Typeable
+import Haxl.Core
+import Network.HTTP.Client
+  ( Manager,
+    defaultManagerSettings,
+    httpLbs,
+    newManager,
+    parseRequest,
+    responseBody,
+  )
+import Network.HTTP.Types.Status (statusCode)
+import Network.Simple.TCP
+import Network.Socket (SockAddr, Socket)
+import Text.Parsec
+import Text.Printf
 
 {-Implementation in Haxl-}
 --
 data ClickHouseQuery a where
-    FetchByteString :: String -> ClickHouseQuery (Maybe BS.ByteString)
-    FetchJSON       :: String -> ClickHouseQuery (Maybe BS.ByteString)
-    FetchCSV        :: String -> ClickHouseQuery (Maybe BS.ByteString)
-    FetchText       :: String -> ClickHouseQuery (Maybe BS.ByteString)
+  FetchByteString :: String -> ClickHouseQuery (Maybe BS.ByteString)
+  FetchJSON :: String -> ClickHouseQuery (Maybe BS.ByteString)
+  FetchCSV :: String -> ClickHouseQuery (Maybe BS.ByteString)
+  FetchText :: String -> ClickHouseQuery (Maybe BS.ByteString)
+
 deriving instance Show (ClickHouseQuery a)
+
 deriving instance Typeable ClickHouseQuery
+
 deriving instance Eq (ClickHouseQuery a)
+
 instance ShowP ClickHouseQuery where showp = show
+
 instance Hashable (ClickHouseQuery a) where
-    hashWithSalt salt (FetchByteString cmd) = hashWithSalt salt cmd
-    hashWithSalt salt (FetchJSON cmd)       = hashWithSalt salt cmd
-    hashWithSalt salt (FetchCSV cmd)        = hashWithSalt salt cmd
+  hashWithSalt salt (FetchByteString cmd) = hashWithSalt salt cmd
+  hashWithSalt salt (FetchJSON cmd) = hashWithSalt salt cmd
+  hashWithSalt salt (FetchCSV cmd) = hashWithSalt salt cmd
+
 instance DataSourceName ClickHouseQuery where
-    dataSourceName _ = "ClickhouseDataSource"
+  dataSourceName _ = "ClickhouseDataSource"
+
 instance DataSource u ClickHouseQuery where
-    fetch (Settings settings) _flags _usrenv = SyncFetch $ \blockedFetches->do
-       printf "Fetching %d queries.\n" (length blockedFetches)
-       res <- mapConcurrently (fetchData settings) blockedFetches
-       case res of
-         [()] -> return ()
+  fetch (Settings settings) _flags _usrenv = SyncFetch $ \blockedFetches -> do
+    printf "Fetching %d queries.\n" (length blockedFetches)
+    res <- mapConcurrently (fetchData settings) blockedFetches
+    case res of
+      [()] -> return ()
+
 instance StateKey ClickHouseQuery where
-    data State ClickHouseQuery = Settings ClickHouseConnection
+  data State ClickHouseQuery = Settings ClickHouseConnection
+
 settings :: ClickHouseConnection -> Haxl.Core.State ClickHouseQuery
 settings = Settings
 
 -- | fetch function
-fetchData :: ClickHouseConnection --Connection configuration
-             ->BlockedFetch ClickHouseQuery --fetched data
-             ->IO()
+fetchData ::
+  ClickHouseConnection -> --Connection configuration
+  BlockedFetch ClickHouseQuery -> --fetched data
+  IO ()
 fetchData settings fetches = do
-    let (queryType, var) = case fetches of
-            BlockedFetch (FetchJSON query) var' -> (repl (query ++ " FORMAT JSON"), var')
-            BlockedFetch (FetchCSV query) var' -> (repl (query ++ " FORMAT CSV"), var')
-            BlockedFetch (FetchByteString query) var' -> (query, var')
-    e <- Control.Exception.try $ do
-        case settings of
-            HttpConnection _ _ _ _ mng->do
-                let url = genUrl settings queryType
-                req <- parseRequest url
-                ans <- responseBody <$> httpLbs req mng
-                return $ Just $ toStrict ans
-            TCPConnection host port _ _->do
-                let prot = genUrl settings queryType
-                (sock, sockaddr) <- connectSock host port
-                send sock prot
-                res <- recv sock 1000
-                return res
-    either (putFailure var) (putSuccess var)
-     (e :: Either SomeException (Maybe BS.ByteString))
+  let (queryType, var) = case fetches of
+        BlockedFetch (FetchJSON query) var' -> (repl (query ++ " FORMAT JSON"), var')
+        BlockedFetch (FetchCSV query) var' -> (repl (query ++ " FORMAT CSV"), var')
+        BlockedFetch (FetchByteString query) var' -> (query, var')
+  e <- Control.Exception.try $ do
+    case settings of
+      HttpConnection _ _ _ _ mng -> do
+        let url = genUrl settings queryType
+        req <- parseRequest url
+        ans <- responseBody <$> httpLbs req mng
+        return $ Just $ toStrict ans
+      TCPConnection host port _ _ -> do
+        let prot = genUrl settings queryType
+        (sock, sockaddr) <- connectSock host port
+        send sock prot
+        res <- recv sock 1000
+        return res
+  either
+    (putFailure var)
+    (putSuccess var)
+    (e :: Either SomeException (Maybe BS.ByteString))
 
 -- | Fetch data from ClickHouse client in the text format.
-getByteString :: String->GenHaxl u w (Maybe BS.ByteString)
+getByteString :: String -> GenHaxl u w (Maybe BS.ByteString)
 getByteString = dataFetch . FetchByteString
 
-getText :: String->GenHaxl u w (Maybe T.Text)
+getText :: String -> GenHaxl u w (Maybe T.Text)
 getText cmd = fmap (fmap decodeUtf8) (getByteString cmd)
 
 -- | Fetch data from ClickHouse client in the JSON format.
-getJSON ::String->GenHaxl u w (Maybe JSONResult)
+getJSON :: String -> GenHaxl u w (Maybe JSONResult)
 getJSON cmd = fmap (fmap extract) (dataFetch $ FetchJSON cmd)
 
 -- | Fetch data from Clickhouse client with commands warped in a Traversable monad.
-getTextM :: (Monad m, Traversable m)=>m String->GenHaxl u w (m (Maybe T.Text))
+getTextM :: (Monad m, Traversable m) => m String -> GenHaxl u w (m (Maybe T.Text))
 getTextM = mapM getText
 
-getJsonM :: (Monad m, Traversable m)=>m String->GenHaxl u w (m (Maybe JSONResult))
+getJsonM :: (Monad m, Traversable m) => m String -> GenHaxl u w (m (Maybe JSONResult))
 getJsonM = mapM getJSON
 
 -- | Default environment
