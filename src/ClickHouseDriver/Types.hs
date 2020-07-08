a264997ca4ac90a27e0238d285f7f59c76655626
Darth <bttfus@gmail.com>
2020-07-07 18:25:02 -0700

    style: use ormolu for formatting

--- src/ClickHouseDriver/Types.hs
+++ src/ClickHouseDriver/Types.hs
@@ -1,40 +1,39 @@
-
 {-# LANGUAGE MultiParamTypeClasses #-}
-module ClickHouseDriver.Types (
-    JSONResult (..),
+
+module ClickHouseDriver.Types
+  ( JSONResult (..),
     ClickHouseConnection (..),
     Cmd,
     Haxl,
-) where
+  )
+where
 
-import           Data.Aeson           (Value)
-import           Data.ByteString      (ByteString)
+import Data.Aeson (Value)
+import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as L
-import           Data.HashMap.Strict  (HashMap)
-import           Data.Text            (Text)
-import           Haxl.Core
-import           Network.HTTP.Client  (Manager)
-
+import Data.HashMap.Strict (HashMap)
+import Data.Text (Text)
+import Haxl.Core
+import Network.HTTP.Client (Manager)
 
 type JSONResult = Either ByteString [HashMap Text Value] --Err LBS.ByteString | OK [HM.HashMap T.Text JP.Value]
-                    --deriving Show
+      --deriving Show
 
-
-data ClickHouseConnection = HttpConnection {
-     httpHost     :: {-# UNPACK #-}  !String
-    ,httpPort     :: {-# UNPACK #-}  !Int
-    ,httpUsername :: {-# UNPACK #-}  !String
-    ,httpPassword :: {-# UNPACK #-}  !String
-    ,httpManager  :: {-# UNPACK #-}  !Manager
-} |
- TCPConnection {
-    tcpHost     :: {-# UNPACK #-} !String
-   ,tcpPort     :: {-# UNPACK #-} !String
-   ,tcpUsername :: {-# UNPACK #-}  !String
-   ,tcpPassword :: {-# UNPACK #-}  !String
-}
-
-
+data ClickHouseConnection
+  = HttpConnection
+      { httpHost :: {-# UNPACK #-} !String,
+        httpPort :: {-# UNPACK #-} !Int,
+        httpUsername :: {-# UNPACK #-} !String,
+        httpPassword :: {-# UNPACK #-} !String,
+        httpManager :: {-# UNPACK #-} !Manager
+      }
+  | TCPConnection
+      { tcpHost :: {-# UNPACK #-} !String,
+        tcpPort :: {-# UNPACK #-} !String,
+        tcpUsername :: {-# UNPACK #-} !String,
+        tcpPassword :: {-# UNPACK #-} !String
+      }
 
 type Cmd = String
+
 type Haxl a = GenHaxl () a
--- test/Test/QuerySpec.hs
+++ test/Test/QuerySpec.hs
@@ -2,15 +2,13 @@
 
 module Test.QuerySpec (spec) where
 
-import           ClickHouseDriver
-import           Data.ByteString       as B
-import           Data.ByteString.Char8 as C8
-import qualified Data.HashMap.Strict        as HM
-import           Haxl.Core
-import           Test.Hspec
-import           Test.HUnit
-
-
+import ClickHouseDriver
+import Data.ByteString as B
+import Data.ByteString.Char8 as C8
+import qualified Data.HashMap.Strict as HM
+import Haxl.Core
+import Test.HUnit
+import Test.Hspec
 
 {-
 query1 = TestCase(do
@@ -28,38 +26,35 @@ query1 = TestCase(do
 tests = TestList [TestLabel "query1" query1]
 -}
 
-
-
-
 spec :: Spec
 spec = parallel $ do
-    query1
-    query2
-    query3
+  query1
+  query2
+  query3
 
 query1 :: Spec
 query1 = describe "show databases" $ do
-    deSetting <- runIO $ defaultHttpConnection 
-    env <- runIO $ setupEnv deSetting
-    res <- runIO $ runQuery env (getByteString "SHOW DATABASES")
-    it "returns query result in text format" $ do
-        res `shouldBe` (Just $ C8.pack "DEMO\n_temporary_and_external_tables\ndefault\nsystem\n")
+  deSetting <- runIO $ defaultHttpConnection
+  env <- runIO $ setupEnv deSetting
+  res <- runIO $ runQuery env (getByteString "SHOW DATABASES")
+  it "returns query result in text format" $ do
+    res `shouldBe` (Just $ C8.pack "DEMO\n_temporary_and_external_tables\ndefault\nsystem\n")
 
 query2 :: Spec
 query2 = describe "format in text" $ do
-    deSetting <- runIO $ defaultHttpConnection
-    env <- runIO $ setupEnv deSetting
-    res <- runIO $ runQuery env (getByteString "SELECT * FROM default.test_table")
-    it "returns query result in text format" $ do
-        res `shouldBe` (Just $ C8.pack "0000000001\tJOHN\t1557\t[45,45,45]\n1234567890\tCONNOR\t533\t[1,2,3,4]\n3543364534\tMARRY\t220\t[0,1,2,3,121,2]\n2258864346\tJAME\t4452\t[42,-10988,66,676,0]\n")
+  deSetting <- runIO $ defaultHttpConnection
+  env <- runIO $ setupEnv deSetting
+  res <- runIO $ runQuery env (getByteString "SELECT * FROM default.test_table")
+  it "returns query result in text format" $ do
+    res `shouldBe` (Just $ C8.pack "0000000001\tJOHN\t1557\t[45,45,45]\n1234567890\tCONNOR\t533\t[1,2,3,4]\n3543364534\tMARRY\t220\t[0,1,2,3,121,2]\n2258864346\tJAME\t4452\t[42,-10988,66,676,0]\n")
 
 query3 :: Spec
 query3 = describe "format in JSON" $ do
-    deSetting <- runIO $ defaultHttpConnection
-    env <- runIO $ setupEnv deSetting
-    res <- runIO $ runQuery env (getJSON "SELECT * FROM default.test_table")
-    let check = case res of
-            Just (Right (x:xs)) -> C8.pack $ show (HM.lookup "id" x)
-            _         -> C8.pack "error"
-    it "returns query result in JSON" $ do
-        check `shouldBe` C8.pack "Just (String \"0000000001\")"
+  deSetting <- runIO $ defaultHttpConnection
+  env <- runIO $ setupEnv deSetting
+  res <- runIO $ runQuery env (getJSON "SELECT * FROM default.test_table")
+  let check = case res of
+        Just (Right (x : xs)) -> C8.pack $ show (HM.lookup "id" x)
+        _ -> C8.pack "error"
+  it "returns query result in JSON" $ do
+    check `shouldBe` C8.pack "Just (String \"0000000001\")"
