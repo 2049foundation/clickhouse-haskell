9e33daf48eef21e45cc3094a04f58b3559cf7e0d
Darth <bttfus@gmail.com>
2020-08-10 23:39:18 -0700

    feat(Core): add enum type

--- test/tableGen.sql
+++ test/tableGen.sql
@@ -1,11 +1,13 @@
-CREATE TABLE IF NOT EXISTS tuple (
+CREATE TABLE IF NOT EXISTS tande (
     `id` Nullable(FixedString(3)),
-    `items` Tuple(Int16, String)
+    `items` Tuple(Int16, String, String, Int8),
+    `x` Enum('hello' = 1, 'world' = 2)
 ) 
 ENGINE = MergeTree()
 ORDER BY tuple();
 
 --INSERT INTO test_table2 (`id`, `item`,`number`,`Strings`) VALUES ('0000000001', Null,1557,['Hello','World','Darling']),('1234567890', 'CONNOR',533,['my', 'dear']),('3543364534', 'MARRY',220,['I', 'Love', 'You']),('2258864346', 'JAME',4452,['who', 'are', 'you'])
 
-INSERT INTO tuple (`id`, `items`) VALUES ('abc', (1, 'John')), ('edf', (2, 'Kanade')), ('xyz', (10, 'Emilia')), ('uvw', (20, 'rem'))
+INSERT INTO tande (`id`, `items`, `x`) VALUES ('abc', (1, 'John', 'good',0), 'hello'), ('edf', (2, 'Kanade', 'bad', 7),'world'), ('xyz', (10, 'Emilia', 'violent', 55),'hello'), ('uvw', (20, 'rem', 'waifu', -12),'world')
+
 
