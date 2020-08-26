bea16b562e3e89eedb0a797310734cce276cd1ac
Darth <bttfus@gmail.com>
2020-08-26 16:33:02 -0700

    style(Core): fix code style

--- src/ClickHouseDriver/Core/Column.hs
+++ src/ClickHouseDriver/Core/Column.hs
@@ -75,44 +75,51 @@ readFixed n_rows spec = do
 readFixedLengthString :: Int -> Reader ClickhouseType
 readFixedLengthString strlen = (CKFixedLengthString strlen) <$> (readBinaryStrWithLength strlen)
 
-readDateTime ::  Int -> ByteString -> Reader (Vector ClickhouseType)
+readDateTime :: Int -> ByteString -> Reader (Vector ClickhouseType)
 readDateTime n_rows spec = do
   let (scale, spc) = readTimeSpec spec
   case spc of
     Nothing -> error "Error : can't read localzone"
     Just tz_name -> readDateTimeWithSpec n_rows scale tz_name
-    where 
-      readTimeSpec :: ByteString -> (Maybe Int, Maybe ByteString)
-      readTimeSpec spec'
-                  | "DateTime64" `isPrefixOf` spec' = do
-                        let l = BS.length spec'
-                        let innerspecs = BS.take (l - 11) (BS.drop 10 spec')
-                        let splited = getSpecs innerspecs
-                        case splited of
-                          [] -> (Nothing, Nothing)
-                          [x] -> (Just $ fst $ fromJust $ readInt x, Just "")
-                          [x, y] -> (Just $ fst $ fromJust $ readInt x, Just y)
-                  | otherwise = do
-                        let l = BS.length spec'
-                        let innerspecs = BS.take (l - 9) (BS.drop 8 spec')
-                        (Nothing, Just innerspecs)
+  where
+    readTimeSpec :: ByteString -> (Maybe Int, Maybe ByteString)
+    readTimeSpec spec'
+      | "DateTime64" `isPrefixOf` spec' = do
+        let l = BS.length spec'
+        let innerspecs = BS.take (l - 11) (BS.drop 10 spec')
+        let splited = getSpecs innerspecs
+        case splited of
+          [] -> (Nothing, Nothing)
+          [x] -> (Just $ fst $ fromJust $ readInt x, Just "")
+          [x, y] -> (Just $ fst $ fromJust $ readInt x, Just y)
+      | otherwise = do
+        let l = BS.length spec'
+        let innerspecs = BS.take (l - 9) (BS.drop 8 spec')
+        (Nothing, Just innerspecs)
+    readDateTimeWithSpec :: Int -> Maybe Int -> ByteString -> Reader (Vector ClickhouseType)
+    readDateTimeWithSpec n_rows Nothing tz_name = do
+      data32 <- readIntColumn n_rows "Int32"
+      let toDateTimeString =
+            V.map
+              ( \(CKInt32 x) ->
+                  formatUnixTimeGMT webDateFormat $
+                    UnixTime (CTime $ fromIntegral x) 0
+              )
+              data32
+      return $ V.map CKString toDateTimeString
+    readDateTimeWithSpec n_rows (Just scl) tz_name = do
+      data64 <- readIntColumn n_rows "Int64"
+      let scale = 10 ^ fromIntegral scl
+      let toDateTimeString =
+            V.map
+              ( \(CKInt64 x) ->
+                  formatUnixTimeGMT webDateFormat $
+                    UnixTime (CTime $ x `div` scale) 0
+              )
+              data64
+      return $ V.map CKString toDateTimeString
 
-      readDateTimeWithSpec :: Int -> Maybe Int->ByteString->Reader (Vector ClickhouseType)
-      readDateTimeWithSpec n_rows Nothing tz_name = do
-        data32 <- readIntColumn n_rows "Int32"
-        let toDateTimeString = V.map (\(CKInt32 x) -> 
-                formatUnixTimeGMT webDateFormat $
-                UnixTime (CTime $ fromIntegral x) 0) data32
-        return $ V.map CKString toDateTimeString
-      readDateTimeWithSpec n_rows (Just scl) tz_name = do
-        data64 <- readIntColumn n_rows "Int64"
-        let scale = 10 ^ fromIntegral scl
-        let toDateTimeString = V.map (\(CKInt64 x) ->
-                formatUnixTimeGMT webDateFormat $
-                UnixTime (CTime $ x `div` scale) 0) data64
-        return $ V.map CKString toDateTimeString
-
-readLowCadinality :: Int->ByteString->Reader (Vector ClickhouseType)
+readLowCadinality :: Int -> ByteString -> Reader (Vector ClickhouseType)
 readLowCadinality 0 _ = return (V.fromList [])
 readLowCadinality n spec = do
   readBinaryUInt64 --prefix
@@ -122,26 +129,25 @@ readLowCadinality n spec = do
   -- Lowest bytes contains info about key type.
   let key_type = serialization_type .&. 0xf
   index_size <- readBinaryUInt64
-  -- Strip the 'Nullable' tag to avoid null map reading. 
+  -- Strip the 'Nullable' tag to avoid null map reading.
   index <- getColumnWithSpec (fromIntegral index_size) (stripNullable inner)
   readBinaryUInt64 -- #keys
   keys <- case key_type of
-            0 ->V.map fromIntegral <$> V.replicateM n readBinaryUInt8
-            1 ->V.map fromIntegral <$>  V.replicateM n readBinaryUInt16
-            2 ->V.map fromIntegral <$>  V.replicateM n readBinaryUInt32
-            3 ->V.map fromIntegral <$>  V.replicateM n readBinaryUInt64
+    0 -> V.map fromIntegral <$> V.replicateM n readBinaryUInt8
+    1 -> V.map fromIntegral <$> V.replicateM n readBinaryUInt16
+    2 -> V.map fromIntegral <$> V.replicateM n readBinaryUInt32
+    3 -> V.map fromIntegral <$> V.replicateM n readBinaryUInt64
   if "Nullable" `isPrefixOf` inner
-    then do 
-      let nullable = fmap (\k->if k == 0 then CKNull else index ! k) keys
+    then do
+      let nullable = fmap (\k -> if k == 0 then CKNull else index ! k) keys
       return nullable
-    else return $ fmap (\k->index ! k) keys
-    where
-      stripNullable :: ByteString->ByteString
-      stripNullable spec
-        | "Nullable" `isPrefixOf` spec = BS.take (l - 10) (BS.drop 9 spec)
-        | otherwise = spec
-        where
-          l = BS.length spec
+    else return $ fmap (\k -> index ! k) keys
+  where
+    stripNullable :: ByteString -> ByteString
+    stripNullable spec
+      | "Nullable" `isPrefixOf` spec = BS.take (l - 10) (BS.drop 9 spec)
+      | otherwise = spec
+    l = BS.length spec
 {-
           Informal description for this config:
           (\Null | \SOH)^{n_rows}
@@ -179,45 +185,41 @@ readNullable n_rows spec = do
       Quoted from https://github.com/mymarilyn/clickhouse-driver/blob/master/clickhouse_driver/columns/arraycolumn.py
 -}
 
-readArray :: Int->ByteString->Reader (Vector ClickhouseType)
+readArray :: Int -> ByteString -> Reader (Vector ClickhouseType)
 readArray n_rows spec = do
-  (lastSpec, x:xs) <- genSpecs spec [V.fromList [fromIntegral n_rows]]
+  (lastSpec, x : xs) <- genSpecs spec [V.fromList [fromIntegral n_rows]]
   let numElem = fromIntegral $ V.sum x
   elems <- getColumnWithSpec numElem lastSpec
-  let result' = foldl combine elems (x:xs)
+  let result' = foldl combine elems (x : xs)
   let result = case (result' ! 0) of
-             CKArray arr -> arr
-             _ -> error "wrong type. This cannot happen"
+        CKArray arr -> arr
+        _ -> error "wrong type. This cannot happen"
   return result
-  where  
+  where
     combine :: Vector ClickhouseType -> Vector Word64 -> Vector ClickhouseType
-    combine elems config = 
+    combine elems config =
       let intervals = intervalize (fromIntegral <$> config)
           cut (a, b) = CKArray $ V.take b (V.drop a elems)
-          embed = (\(l, r)->cut (l, r - l + 1)) <$> intervals
-      in  embed
-        
+          embed = (\(l, r) -> cut (l, r - l + 1)) <$> intervals
+       in embed
     intervalize :: Vector Int -> Vector (Int, Int)
-    intervalize vec = V.drop 1 $ V.scanl' (\(a, b) v->(b+1, v+b)) (-1, -1) vec
-
-    readArraySpec :: Vector Word64->Reader (Vector Word64)
+    intervalize vec = V.drop 1 $ V.scanl' (\(a, b) v -> (b + 1, v + b)) (-1, -1) vec
+    readArraySpec :: Vector Word64 -> Reader (Vector Word64)
     readArraySpec sizeArr = do
       let arrSum = (fromIntegral . V.sum) sizeArr
       offsets <- V.replicateM arrSum readBinaryUInt64
       let offsets' = V.cons 0 (V.take (arrSum - 1) offsets)
       let sizes = V.zipWith (-) offsets offsets'
       return sizes
-
-    genSpecs :: ByteString->[Vector Word64]-> Reader (ByteString, [Vector Word64])
-    genSpecs spec rest@(x:xs) = do
+    genSpecs :: ByteString -> [Vector Word64] -> Reader (ByteString, [Vector Word64])
+    genSpecs spec rest@(x : xs) = do
       let l = BS.length spec
       let cktype = BS.take (l - 7) (BS.drop 6 spec)
       if "Array" `isPrefixOf` spec
-        then do 
+        then do
           next <- readArraySpec x
-          genSpecs cktype (next:rest)
-        else
-          return (spec, rest)
+          genSpecs cktype (next : rest)
+        else return (spec, rest)
 
 readTuple :: Int->ByteString->Reader (Vector ClickhouseType)
 readTuple n_rows spec = do
@@ -229,24 +231,25 @@ readTuple n_rows spec = do
   return $ CKTuple <$> transposed
 
 
-readEnum :: Int->ByteString->Reader (Vector ClickhouseType)
+readEnum :: Int -> ByteString -> Reader (Vector ClickhouseType)
 readEnum n_rows spec = do
   let l = BS.length spec
-      innerSpec = if "Enum8" `isPrefixOf` spec 
-        then BS.take(l - 7) (BS.drop 6 spec)
-        else BS.take(l - 8) (BS.drop 7 spec)
+      innerSpec =
+        if "Enum8" `isPrefixOf` spec
+          then BS.take (l - 7) (BS.drop 6 spec)
+          else BS.take (l - 8) (BS.drop 7 spec)
       prespecs = getSpecs innerSpec
-      specs = (\(name, Just (n, _))-> (n, name)) <$> ((toTuple . BS.splitWith (== 61)) <$> prespecs) --61 is '='
+      specs = (\(name, Just (n, _)) -> (n, name)) <$> ((toTuple . BS.splitWith (== 61)) <$> prespecs) --61 is '='
       specsMap = Map.fromList specs
-  if "Enum8" `isPrefixOf` spec 
-    then do 
+  if "Enum8" `isPrefixOf` spec
+    then do
       vals <- V.replicateM n_rows readBinaryInt8
       return $ (CKString . (specsMap Map.!) . fromIntegral) <$> vals
     else do
       vals <- V.replicateM n_rows readBinaryInt16
       return $ (CKString . (specsMap Map.!) . fromIntegral) <$> vals
-      where
-        toTuple [x, y] = (x, readInt y)
+  where
+    toTuple [x, y] = (x, readInt y)
 
 readDate :: Int->ByteString->Reader(Vector ClickhouseType)
 readDate n_rows spec = do
@@ -261,9 +264,7 @@ readDecimal :: Int->ByteString->Reader(Vector ClickhouseType)
 readDecimal n_rows spec = do
   let l = BS.length spec 
   let [precision', scale'] = getSpecs $ BS.take(l - 9) (BS.drop 8 spec)
-  
   let (Just (precision,_), Just (scale,_)) = (readInt precision', readInt scale')
-
   let specific = 
         if precision <= 9
           then readDecimal32
@@ -272,15 +273,12 @@ readDecimal n_rows spec = do
             else readDecimal128
   
   raw <- specific n_rows
-
   let final = fmap (trans scale) raw
-  
   return final
   where
     readDecimal32 n_rows = readIntColumn n_rows "Int32"
     readDecimal64 n_rows = readIntColumn n_rows "Int64"
     readDecimal128 n_rows = undefined
-
     trans :: Int->ClickhouseType->ClickhouseType
     trans scale (CKInt32 x) = CKDecimal32 (fromIntegral x / fromIntegral scale)
     trans scale (CKInt64 x) = CKDecimal64 (fromIntegral x / fromIntegral scale)
