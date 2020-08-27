1b43891f46c71b21e910ef90bf9675acff1c2eb6
Darth <bttfus@gmail.com>
2020-08-26 21:51:28 -0700

    fix(HTTP): remove redundant codes

--- src/ClickHouseDriver/Core/HTTP/Types.hs
+++ src/ClickHouseDriver/Core/HTTP/Types.hs
@@ -13,8 +13,7 @@ import Data.HashMap.Strict (HashMap)
 import Data.Text (Text)
 import Haxl.Core
 
-type JSONResult = Either ByteString [HashMap Text Value] --Err LBS.ByteString | OK [HM.HashMap T.Text JP.Value]
-      --deriving Show
+type JSONResult = Either ByteString [HashMap Text Value]
 
 type Cmd = String
 
