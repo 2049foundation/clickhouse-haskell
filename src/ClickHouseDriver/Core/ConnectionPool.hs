a527e494eac3da5a07f9533cdeafafb6efbbe283
Darth <bttfus@gmail.com>
2020-09-30 07:32:37 -0700

    feat(Core): add connection pool creation

--- src/ClickHouseDriver/Core/ConnectionPool.hs
+++ src/ClickHouseDriver/Core/ConnectionPool.hs
@@ -1,20 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
 module ClickHouseDriver.Core.ConnectionPool () where
 
-import ClickHouseDriver.Core.Connection
-import ClickHouseDriver.Core.Types
-import Data.Default.Class
-import Network.Socket
-import Data.Streaming.Network
-import Data.ConnectionPool
-import Data.ConnectionPool.Internal.ResourcePoolParams
+import           ClickHouseDriver.Core.Connection
+import           ClickHouseDriver.Core.Types
+import           Data.ConnectionPool
+import           Data.ConnectionPool.Internal.ResourcePoolParams
+import           Data.Default.Class
+import           Data.Streaming.Network
+import           Network.Socket
+import           Data.Time.Clock
 
-createClickhousePool :: IO (ConnectionPool TcpClient)
-createClickhousePool = undefined
+createClickhousePool :: Int
+                      ->NominalDiffTime
+                      ->Int
+                      ->IO (ConnectionPool TcpClient)
+createClickhousePool numberOfStripes resourceIdleTimeout numberOfResourcesPerStripe
+    = createTcpClientPool 
+        (ResourcePoolParams 
+         numberOfStripes
+         resourceIdleTimeout 
+         numberOfResourcesPerStripe)
+        (clientSettingsTCP 9000 "127.0.0.1")
 
 testing :: IO ()
 testing = do
-    pool <- createTcpClientPool 
-        (ResourcePoolParams 1 0.5 1) 
-        (clientSettingsTCP 8123 "127.0.0.1")
+    pool <- createTcpClientPool
+        (ResourcePoolParams 1 0.5 1)
+        (clientSettingsTCP 9000 "127.0.0.1")
     print "pool"
