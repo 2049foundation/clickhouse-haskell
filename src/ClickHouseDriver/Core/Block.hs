8299b152444a76df3b6b54dfec5441d9fca1afe4
Darth <bttfus@gmail.com>
2020-10-17 15:38:25 -0700

    style(Core/Block): delete redundant spaces

--- src/ClickHouseDriver/Core/Block.hs
+++ src/ClickHouseDriver/Core/Block.hs
@@ -52,7 +52,6 @@ writeInfo (Info is_overflows bucket_num) = do
     writeBinaryInt32 bucket_num
     writeVarUInt 0
 
-
 readInfo :: BlockInfo -> Reader BlockInfo
 readInfo info@Info {is_overflows = io, bucket_num = bn} = do
   field_num <- readVarInt
@@ -74,26 +73,18 @@ readBlockInputStream = do
             bucket_num = -1
           } -- TODO should have considered the revision
   info <- readInfo defaultInfo
-
   n_columns <- readVarInt
   n_rows <- readVarInt
-
-  --(datas, names, types)
   let loop :: Int -> Reader (Vector ClickhouseType, ByteString, ByteString)
       loop n = do
         column_name <- readBinaryStr
         column_type <- readBinaryStr
-
         column <- readColumn (fromIntegral n_rows) column_type
-
         return (column, column_name, column_type)
-
   v <- V.generateM (fromIntegral n_columns) loop
-
   let datas = (\(x, _, _) -> x) <$> v
       names = (\(_, x, _) -> x) <$> v
       types = (\(_, _, x) -> x) <$> v
-
   return
     ColumnOrientedBlock
       { cdata = datas,
