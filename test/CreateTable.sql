81f1215a5c1d4b62ef3d76baacb068403f032917
Shi You <bttfus@gmail.com>
2020-12-03 00:05:13 -0800

    update version number

--- test/CreateTable.sql
+++ test/CreateTable.sql
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-CREATE TABLE IF NOT EXISTS default.test_table (
-    `id` FixedString(10),
-    `item` String,
-    `number` Int16,
-    `numArray` Array(Int16)
-) ENGINE = MergeTree()
-ORDER BY tuple();
-
-
-
-=======
 CREATE TABLE IF NOT EXISTS nulls_table (
     `id` Nullable(FixedString(10)),
     `item` Nullable(String),
@@ -18,5 +6,4 @@ CREATE TABLE IF NOT EXISTS nulls_table (
 ENGINE = MergeTree()
 ORDER BY tuple();
 
->>>>>>> 380b061d9d16ed42724f5da6b8a080b8ade86777
 
