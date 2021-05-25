b2b2d22a2bcf2c2a06790694f9725e75125ad044
MaboroshiChan <bttfus@gmail.com>
2021-05-25 14:26:36 +0800

    doc(IO): add fix docs

--- src/Database/ClickHouseDriver/Include/varuint.h
+++ src/Database/ClickHouseDriver/Include/varuint.h
@@ -12,11 +12,10 @@
 const char * write_varint(__uint16_t number);
 
 /**
- * @
  * @param __uint16_t intermediate result to continue, which takes an uint16
  * @param char* string buffer, which takes an char pointer 
  * @param size_t size of the string buffer, which takes size_t
- * @return the read varuint from string buffer. 
+ * @return the read varuint from string buffer combined with offset information. 
  **/
 __uint32_t read_varint(__uint16_t cont,char *istr, size_t size);
 
